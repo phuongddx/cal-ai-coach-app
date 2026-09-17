f572c9944ebf37dbae94926b323053c1c35426d1
PhuongDoan <95doanphuong@gmail.com>
2026-09-17 23:07:56 +0700

    feat(native): bootstrap Tuist package manifest

---
+++ native/Tuist/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+#if TUIST
+    import ProjectDescription
+
+    let packageSettings = PackageSettings(
+        productTypes: [:]
+    )
+#endif
+
+let package = Package(
+    name: "CoachCal",
+    dependencies: [
+        .package(path: "../Modules/CoachCalCore"),
+        .package(path: "../Modules/CoachCalDesignSystem"),
+        .package(path: "../Modules/CoachCalPersistence"),
+        .package(path: "../Modules/CoachCalNetworking"),
+        .package(path: "../Modules/CoachCalSync"),
+        .package(url: "https://github.com/nalexn/ViewInspector", exact: "0.10.3"),
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.19.4"),
+        .package(url: "https://github.com/getsentry/sentry-cocoa", exact: "9.28.0"),
+        .package(url: "https://github.com/PostHog/posthog-ios", exact: "3.75.2"),
+    ]
+)
